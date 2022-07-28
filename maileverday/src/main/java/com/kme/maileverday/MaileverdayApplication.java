81f22e1140163e03fd43d5f9fec0784eaf168cfc
YOUNJEI <kyj951211@gmail.com>
2022-07-28 11:17:20 +0900

    git init

---
+++ maileverday/src/main/java/com/kme/maileverday/MaileverdayApplication.java
@@ -0,0 +1,13 @@
+package com.kme.maileverday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaileverdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaileverdayApplication.class, args);
+	}
+
+}
