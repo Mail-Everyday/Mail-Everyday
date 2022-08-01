330b1fe02241e33475205baceb20519d6236ded8
YOUNJEI <kyj951211@gmail.com>
2022-08-01 22:04:39 +0900

    Feat : Keywords 프론트 페이지 제작

--- maileverday/src/main/java/com/kme/maileverday/web/IndexController.java
+++ maileverday/src/main/java/com/kme/maileverday/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.kme.maileverday.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +10,10 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/keywords")
+    public String keywordsList() {
+        return "keywords";
     }
 }
--- maileverday/src/main/java/com/kme/maileverday/web/UserController.java
+++ maileverday/src/main/java/com/kme/maileverday/web/UserController.java
@@ -4,8 +4,12 @@ import com.kme.maileverday.service.GoogleUserService;
 import com.kme.maileverday.utility.EnvironmentKey;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -13,7 +17,7 @@ public class UserController {
     private final GoogleUserService googleUserService;
 
     @GetMapping("/login/google")
-    public String loginGoogle(@RequestParam(value = "code", required = false) String authCode) {
+    public String loginGoogle(@RequestParam(value = "code", required = false) String authCode, HttpSession session) {
         if (authCode == null) {
             return "redirect:"
                     + "https://accounts.google.com/o/oauth2/v2/auth?"
@@ -26,6 +30,7 @@ public class UserController {
         else {
             try {
                 String userName = googleUserService.login(authCode);
+                session.setAttribute("userName", userName);
             } catch (Exception e) {
                 e.printStackTrace();
             }
