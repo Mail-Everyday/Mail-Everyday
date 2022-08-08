f21a45e9b1a0f917ead537836473ccfb1c59468e
YOUNJEI <kyj951211@gmail.com>
2022-08-08 21:28:18 +0900

    Fix : 페이지 접속권한 예외처리 방식 변경 (템플릿 엔진쪽으로 위임)

--- maileverday/src/main/java/com/kme/maileverday/web/IndexController.java
+++ maileverday/src/main/java/com/kme/maileverday/web/IndexController.java
@@ -3,7 +3,6 @@ package com.kme.maileverday.web;
 import com.kme.maileverday.service.KeywordService;
 import com.kme.maileverday.service.ResponseService;
 import com.kme.maileverday.utility.exception.CustomException;
-import com.kme.maileverday.utility.exception.ErrorMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +14,6 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class IndexController {
     private final KeywordService keywordService;
-    private final ResponseService responseService;
 
     @GetMapping("/")
     public String index() {
@@ -26,44 +24,15 @@ public class IndexController {
     public String keywordsList(HttpSession session, Model model) {
         String userEmail = (String) session.getAttribute("userEmail");
 
-        if (userEmail != null) {
-            // 모델로 넘겨줄 키 값
-            final String responseKey = "ListResponse";
-
-            try {
-                // 서비스 정상 작동 시
-                // 유저키워드 엔티티 (List), 200 (상태코드), true (성공여부), OK (메시지) 를 내려줌
-                model.addAttribute(responseKey,
-                        responseService.getListResponse(keywordService.findAllAsc(userEmail), 200,
-                                true, ErrorMessage.OK.getDesc()));
-            } catch (CustomException e) {
-                // 서비스 예외 발생 시
-                // 유저키워드 엔티티 (null), (상태코드), false (성공여부), (메시지) 를 내려줌
-                if (e.getCode().equals(ErrorMessage.USER_EMAIL_NOT_FOUND.getCode())) {
-                    model.addAttribute(responseKey,
-                            responseService.getListResponse(null, 403, false, ErrorMessage.USER_EMAIL_NOT_FOUND.getDesc()));
-                }
-                else {
-                    model.addAttribute(responseKey,
-                            responseService.getListResponse(null, 500, false, e.toString()));
-                }
-            }
-            return "keywords";
+        try { model.addAttribute("keywords", keywordService.findAllAsc(userEmail)); }
+        catch (CustomException e) {
+            model.addAttribute("error", e.getCode());
         }
-        else {
-            // 잘못된 접근 (로그인이 되지 않은 상태에서 접속 시도)
-            return "permissionalert";
-        }
+        return "keywords";
     }
 
     @GetMapping("/keywords/save")
-    public String keywordsSave(HttpSession session) {
-        if (session.getAttribute("userEmail") != null) {
-            return "keywords-save";
-        }
-        else {
-            // 잘못된 접근 (로그인이 되지 않은 상태에서 접속 시도)
-            return "permissionalert";
-        }
+    public String keywordsSave() {
+        return "keywords-save";
     }
 }
